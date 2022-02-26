1925216c90918fd4d2f01c266acf8109411afc94
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-02-26 14:16:38 +0300

    X2: config.fs: Update from X3 tree https://github.com/ghostrider-reborn/android_device_realme_x3/blob/12/configs/fs/config.fs

--- config.fs
+++ config.fs
@@ -1,11 +1,29 @@
 [AID_VENDOR_QTI_DIAG]
 value:2901
 
+[AID_VENDOR_QDSS]
+value:2902
+
 [AID_VENDOR_RFS]
 value:2903
 
 [AID_VENDOR_RFS_SHARED]
 value:2904
+
+[AID_VENDOR_ADPL_ODL]
+value:2905
+
+[AID_VENDOR_QRTR]
+value:2906
+
+[AID_VENDOR_THERMAL]
+value:2907
+
+[AID_VENDOR_FASTRPC]
+value:2908
+
+[AID_VENDOR_QTR]
+value:2909
 
 [vendor/bin/wcnss_filter]
 mode: 0755
@@ -46,6 +64,12 @@ caps: NET_BIND_SERVICE
 [vendor/bin/imsrcsd]
 mode: 0755
 user: AID_SYSTEM
+group: AID_RADIO
+caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
+
+[vendor/bin/imsdaemon]
+mode: 0755
+user: AID_RADIO
 group: AID_RADIO
 caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
 
