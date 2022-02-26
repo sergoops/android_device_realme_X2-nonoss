a5328df65fb976f6f8593ab2f7f1b08ca7871df8
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-02-26 13:58:21 +0300

    X2: init: Update from X3 tree [@sergoops] Adapt for X2

--- init/x2.cpp
+++ init/x2.cpp
@@ -1,211 +1,102 @@
 /*
- * Copyright (C) 2020 LineageOS Project
- * Copyright (C) 2020 Harshit Jain
- *
- * SPDX-License-Identifier: Apache-2.0
+   Copyright (c) 2015, The Linux Foundation. All rights reserved.
+   Copyright (C) 2016 The CyanogenMod Project.
+   Copyright (C) 2019-2020 The LineageOS Project.
+   Copyright (C) 2021 ArrowOS.
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are
+   met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+    * Neither the name of The Linux Foundation nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+   THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+   ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+   BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <vector>
-#include <string>
+#include <cstdlib>
+#include <string.h>
 #include <fstream>
 
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
-
 #include <android-base/properties.h>
-#include <android-base/logging.h>
 
 #include "property_service.h"
 #include "vendor_init.h"
 
-struct x2_props
-{
-    std::string build_description;
-    std::string build_fingerprint;
-    std::string device_build;
-    std::string product_device;
-};
+using android::base::GetProperty;
+using std::string;
 
-std::vector<std::string> ro_props_default_source_order = {
-    "",
-    "odm.",
-    "product.",
-    "system.",
-    "system_ext.",
-    "vendor.",
-};
+void property_override(char const prop[], char const value[]) {
+    auto pi = (prop_info*) __system_property_find(prop);
 
-static const char *snet_prop_key[] = {
-    "ro.boot.vbmeta.device_state",
-    "ro.boot.verifiedbootstate",
-    "ro.boot.flash.locked",
-    "ro.boot.selinux",
-    "ro.boot.veritymode",
-    "ro.boot.warranty_bit",
-    "ro.warranty_bit",
-    "ro.debuggable",
-    "ro.secure",
-    "ro.build.type",
-    "ro.build.tags",
-    "ro.build.selinux",
-    NULL
-};
-
-static const char *snet_prop_value[] = {
-    "locked",
-    "green",
-    "1",
-    "enforcing",
-    "enforcing",
-    "0",
-    "0",
-    "0",
-    "1",
-    "user",
-    "release-keys",
-    "0",
-    NULL
-};
-
-bool isCN()
-{
-    // Get region
-    std::ifstream infile("/proc/oppoVersion/region");
-    std::string region;
-    bool ret = false;
-    getline(infile, region);
-    if (!region.compare("China"))
-        ret = true;
-    return ret;
-}
-
-bool hasNFC()
-{
-    // Check NFC
-    std::ifstream infile("/proc/touchpanel/NFC_CHECK");
-    std::string check;
-    bool ret = false;
-    getline(infile, check);
-    if (!check.compare("SUPPORTED"))
-        ret = true;
-    return ret;
-}
-
-// Make life easier
-bool isRMX1993()
-{
-    return (!isCN() && hasNFC());
-}
-
-void property_override(char const prop[], char const value[], bool add = true)
-{
-    prop_info *pi;
-    pi = (prop_info *)__system_property_find(prop);
-    if (pi)
+    if (pi != nullptr)
         __system_property_update(pi, value, strlen(value));
-    else if (add)
+    else
         __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
-static void workaround_snet_properties() {
-    for (int i = 0; snet_prop_key[i]; ++i)
-        property_override(snet_prop_key[i], snet_prop_value[i]);
-}
+void set_ro_build_prop(const string &source, const string &prop,
+                        const string &value, bool product = true) {
+    string prop_name;
 
-// Decommonise nfc properties
-void load_nfc_props()
-{
-    property_override("persist.sys.nfc.disPowerSave", "1");
-    property_override("persist.sys.nfc.default", "on");
-    property_override("persist.sys.nfc.aid_overflow", "true");
-    property_override("ro.product.cuptsm", "OPPO|ESE|01|02");
-    property_override("persist.sys.nfc.antenna_area", "bottom");
-}
-
-void setRMX(const unsigned int variant)
-{
-    x2_props prop[3] = {};
-
-    //RMX1991
-    prop[0] = {
-        "realme/RMX1991/RMX1991CN:10/QKQ1.191201.002/1599815101:user/release-keys",
-        "unknown-user 10 QKQ1.191201.002 eng.root.20201028.204016 release-keys",
-        "RMX1991",
-        "RMX1991CN",
-    };
-
-    //RMX992
-    prop[1] = {
-        "realme/RMX1992/RMX1992L1:10/QKQ1.191201.002/1597316082:user/release-keys",
-        "unknown-user 10 QKQ1.191201.002 eng.root.20201012.094803 release-keys",
-        "RMX1992",
-        "RMX1992L1",
-    };
-
-    //RMX1993
-    prop[2] = {
-        "realme/RMX1993EEA/RMX1993L1:10/QKQ1.191201.002/1597316082:user/release-keys",
-        "unknown-user 10 QKQ1.191201.002 eng.root.20201012.094557 release-keys",
-        "RMX1993",
-        "RMX1993L1",
-    };
-
-    const auto set_ro_build_prop = [](const std::string &source,
-                                      const std::string &prop, const std::string &value) {
-        auto prop_name = "ro." + source + "build." + prop;
-        property_override(prop_name.c_str(), value.c_str(), false);
-    };
-
-    const auto set_ro_product_prop = [](const std::string &source,
-                                        const std::string &prop, const std::string &value) {
-        auto prop_name = "ro.product." + source + prop;
-        property_override(prop_name.c_str(), value.c_str(), false);
-    };
-
-    property_override("ro.build.description", prop[variant].build_fingerprint.c_str());
-    property_override("ro.build.product", prop[variant].product_device.c_str());
-    property_override("ro.bootimage.build.fingerprint", prop[variant].build_description.c_str());
-    property_override("ro.vendor.oppo.fingerprint", prop[variant].build_description.c_str());
-    for (const auto &source : ro_props_default_source_order)
-    {
-        set_ro_build_prop(source, "fingerprint", prop[variant].build_description.c_str());
-        set_ro_product_prop(source, "device", prop[variant].product_device.c_str());
-        set_ro_product_prop(source, "model", prop[variant].device_build.c_str());
-        set_ro_product_prop(source, "name", prop[variant].device_build.c_str());
-    }
-
-    // oppo blobs are setting this to true, which kills the macro and other lenses.
-    property_override("ro.vendor.gsi.build.flavor", "none");
-
-    // RMX1993 has different ptoduct name due to oversea variants further being divided into spain and europe
-    if (variant == 2)
-        for (const auto &source : ro_props_default_source_order)
-            set_ro_product_prop(source, "name", "RMX1993EEA"); //override name props again
-
-    //override again to be sure that it gets set.
-    property_override("ro.build.fingerprint", prop[variant].build_description.c_str(), false);
-
-    // Disable apex updatable
-    property_override("ro.apex.updatable", "false");
-
-    // Load NFC properties only on RMX199{1:3}
-    if (variant == 2 || variant == 0)
-        load_nfc_props();
-}
-
-void vendor_load_properties()
-{
-    workaround_snet_properties();
-    if (isRMX1993())
-    {
-        setRMX(2); //RMX1993
-    }
-    else if (!isCN())
-    {
-        setRMX(1); //RMX1992
-    }
+    if (product)
+        prop_name = "ro.product." + source + prop;
     else
-    {
-        setRMX(0); //RMX1991
+        prop_name = "ro." + source + "build." + prop;
+
+    property_override(prop_name.c_str(), value.c_str());
+}
+
+void vendor_load_properties() {
+    /*
+     * Detect device and configure properties
+     */
+    std::ifstream infile("/proc/oplusVersion/operatorName");
+    string operatorName;
+    string device;
+    string model;
+
+    getline(infile, operatorName);
+
+    switch (stoi(operatorName)) {
+        case 8: // 1991
+            device = "RMX1991CN";
+            model = "RMX1991";
+            break;
+        case 31: // 1992
+            device = "RMX1992L1";
+            model = "RMX1992";
+            break;
+        default: // 1993
+            device = "RMX1993L1";
+            model = "RMX1993";
+            break;
+    }
+
+    // list of partitions to override props
+    string source_partitions[] = { "", "bootimage.", "odm.", "product.",
+                                   "system.", "system_ext.", "vendor." };
+
+    for (const string &source : source_partitions) {
+        set_ro_build_prop(source, "device", device);
+        set_ro_build_prop(source, "product", model, false);
+        set_ro_build_prop(source, "model", model);
+        set_ro_build_prop(source, "name", model);
     }
 }
