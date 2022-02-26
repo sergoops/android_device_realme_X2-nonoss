5ff4500396a3333fa2907e894da5ca22828dbc3b
sergoops <sergoopsmoskal@gmail.com>
2022-02-26 13:45:43 +0300

    config.fs from X3

--- config.fs
+++ config.fs
@@ -7,6 +7,21 @@ value:2903
 [AID_VENDOR_RFS_SHARED]
 value:2904
 
+[AID_VENDOR_ADPL_ODL]
+value:2905
+
+[AID_VENDOR_QRTR]
+value:2906
+
+[AID_VENDOR_THERMAL]
+value:2907
+
+[AID_VENDOR_FASTRPC]
+value:2908
+
+[AID_VENDOR_QTR]
+value:2909
+
 [vendor/bin/wcnss_filter]
 mode: 0755
 user: AID_BLUETOOTH
@@ -54,6 +69,12 @@ mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE BLOCK_SUSPEND NET_ADMIN
+
+[vendor/bin/imsdaemon]
+mode: 0755
+user: AID_RADIO
+group: AID_RADIO
+caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
 
 [vendor/bin/slim_daemon]
 mode: 0755
